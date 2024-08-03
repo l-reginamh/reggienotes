8a4b45a5ef43d65e6d2eb0666b0059c66164ae52
NIKLAUS_REG\mhlie <reginaliew9@gmail.com>
2024-08-03 20:28:41 +0800

    Add Command Palette using Kbar for search purpose

--- components/icons.tsx
+++ components/icons.tsx
@@ -47,5 +47,21 @@ export const Icons = {
         >
             <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/>
         </svg>
+    ),
+    search: (props: IconProps) => (
+        <svg
+          className="text-gray-800 dark:text-gray-300"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
     )
 }
--- components/siteHeader.tsx
+++ components/siteHeader.tsx
@@ -5,10 +5,99 @@ import { SideNav } from "./nav/sideNav";
 import { MobileNav } from "./nav/mobileNav";
 import { usePathname, useRouter } from "next/navigation";
 import { Button } from "./ui/button";
+import CommandBar from "./commandBar";
+import { posts, cheatsheets } from "../.velite"
+import { KBarProvider, Priority, useKBar } from "kbar";
+import { siteConfig } from "@/config/site";
+import { useCallback, useMemo, useState } from "react";
+import { SearchBar } from "./nav/searchBar";
+import CommandPortal from "./commandPortal";
+
+const ACTION_KEY_DEFAULT = "CTRL"
+const ACTION_KEY_APPLE = "⌘"
+function isAppleDevice() {
+  return /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)
+}
 
 export function SiteHeader() {
     const pathname = usePathname()
     const router = useRouter()
+    
+    const key = useMemo(() => {
+      if (typeof navigator !== "undefined") {
+        return isAppleDevice() ? ACTION_KEY_APPLE : ACTION_KEY_DEFAULT
+      }
+    }, [])
+
+    function getActions() {
+        const actions = [
+            {
+              id: "homepage",
+              name: "Homepage",
+              keywords: "",
+              section: "Home",
+              perform: () => router.push("/"),
+              priority: Priority.HIGH,
+            },
+            {
+              id: "posts",
+              name: "Posts",
+              keywords: "blog posts content notes",
+              section: "Home",
+              perform: () => router.push("/posts"),
+              priority: Priority.HIGH,
+            },
+            {
+              id: "cheatsheets",
+              name: "Cheatsheets",
+              keywords: "sheets cheatsheets cheatsheet notes",
+              section: "Home",
+              perform: () => router.push("/cheatsheets"),
+              priority: Priority.HIGH,
+            },
+            {
+              id: "twitter",
+              name: "Twitter",
+              keywords: "contact twitter tweet",
+              section: "Contact",
+              perform: () => window.open(`${siteConfig.links.twitter}`, "_blank"),
+              priority: Priority.HIGH,
+            },
+            {
+              id: "github",
+              name: "GitHub",
+              keywords: "contact git GitHub projects showcase repo",
+              section: "Contact",
+              perform: () => window.open(`${siteConfig.links.github}`, "_blank"),
+              priority: Priority.HIGH,
+            },
+        ]
+    
+        for (const post of posts) {
+          actions.push({
+                id: post.slugAsParams,
+                name: post.title,
+                keywords: post?.title || "",
+                section: "Posts",
+                perform: () => router.push("/" + post.slug),
+                priority: Priority.NORMAL
+              })
+        }
+    
+        for (const sheet of cheatsheets) {
+          actions.push({
+                id: sheet.slugAsParams,
+                name: sheet.title,
+                keywords: sheet?.title || "",
+                section: "Cheatsheets",
+                perform: () => router.push("/" + sheet.slug),
+                priority: Priority.NORMAL
+              })
+        }
+    
+        return actions;
+    }
+
 
     return (
         <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filyer]:bg-background/60">
@@ -18,6 +107,48 @@ export function SiteHeader() {
                     :
                     null
                 }
+                <KBarProvider actions={getActions()}>
+                    <SearchBar
+                        onClick={() => {
+                            const ctrlkey = new KeyboardEvent("keydown", {altKey:false,
+                                bubbles: true,
+                                cancelable: true,
+                                charCode: 0,
+                                code: "Ctrl",
+                                composed: true,
+                                ctrlKey: true,
+                                detail: 0,
+                                isComposing: false,
+                                key: "Ctrl",
+                                keyCode: 17,
+                                location: 0,
+                                metaKey: false,
+                                repeat: false,
+                                shiftKey: false,
+                                which: 17});
+                            const kKey = new KeyboardEvent("keydown", {altKey:false,
+                                bubbles: true,
+                                cancelable: true,
+                                charCode: 0,
+                                code: "k",
+                                composed: true,
+                                ctrlKey: true,
+                                detail: 0,
+                                isComposing: false,
+                                key: "k",
+                                keyCode: 107,
+                                location: 0,
+                                metaKey: false,
+                                repeat: false,
+                                shiftKey: false,
+                                which: 107});
+                            document.dispatchEvent(ctrlkey);
+                            document.dispatchEvent(kKey);
+                        }}
+                        kbd={key}
+                    />
+                    <CommandPortal actions={getActions()} />
+                </KBarProvider>
                 <nav className="flex flex-1 items-center sm:justify-center space-x-4 lg:space-x-6">
                     <MainNav />
                 </nav>
